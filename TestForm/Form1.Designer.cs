a596d65b5a1ef9b321643d65ae7b3cf31447be9d
phanmanhcuong <phancuong1203@gmail.com>
2018-01-31 22:08:02 +0700

    Add project files.

---
+++ TestForm/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(435, 316);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose a device";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Choose date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(157, 84);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(157, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 5;
+            //this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1.SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(157, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 27);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Read and send data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 316);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Test Form";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ TestForm/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddDeviceToListBox();
+        }
+
+        private void AddDeviceToListBox()
+        {
+            string directoryPath = "C:\\Users\\Admin\\DevicesLogFiles";
+            string [] devicesLogFiles = Directory.GetFiles(directoryPath);
+            foreach(string deviceLogFile in devicesLogFiles)
+            {
+                string deviceIMEI = Path.GetFileNameWithoutExtension(deviceLogFile);
+                comboBox1.Items.Add(deviceIMEI);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line;
+            string deviceLogFilePath = "C:\\Users\\Admin\\DevicesLogFiles\\" + comboBox1.Text + ".txt";
+            StreamReader deviceLogFile = new StreamReader(deviceLogFilePath);
+            while((line = deviceLogFile.ReadLine()) != null)
+            {
+                String lineData = deviceLogFile.ReadLine();
+                SendData(lineData);
+                
+            }
+        }
+
+        private void SendData(string postData)
+        {
+            //send data
+            WebRequest request = WebRequest.Create("http://localhost:80");
+            request.Method = "POST";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            //acttract data
+            //string[] cmd = strData.Split(',');
+            //if (cmd.Length >= 16)
+            //{
+            //    string imei = cmd[0].Substring(1);
+            //    string strReceivedTime = cmd[1];
+            //    string strLon = cmd[2];
+            //    string strLat = cmd[3];
+            //    string strSpeed = cmd[4];
+            //    string strMNC = cmd[5].Substring(1, cmd[5].Length - 2); //cellID: "32CB"
+            //    string strLAC = cmd[6].Substring(1, cmd[6].Length - 2); //LacID: "32CB"
+
+            //    string strSOS = cmd[7]; //co sos
+            //    string strIsStrongboxOpen = cmd[8]; //co mo ket: dong/mo
+            //    string strIsEngineOn = cmd[9]; //co dong co bat/tat
+            //    string strIsStoping = cmd[10]; //co dung do dung/do
+            //    string strIsGPSLost = cmd[11]; //co GPS mat/co
+            //    string strTotalImageCam1 = cmd[12];
+            //    string strTotalImageCam2 = cmd[13];
+            //    string strRFID = cmd[14];
+            //    string strOBD = cmd[15];
+            //    string strVersion = cmd[cmd.Length - 2];
+            //}
+
+        }
+    }
+}
